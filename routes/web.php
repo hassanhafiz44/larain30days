f2520cc02af0baadec21c9550de1bf43c6b22993
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2026-01-04 10:28:28 +0500

    Day 1 completed
    - Added a new view file without blade
    - Loading Hello world
    - Added a few routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
+use SebastianBergmann\FileIterator\Factory;
 
-Route::get('/', function () {
+Route::get('/', function (): Factory|View {
     return view('welcome');
+});
+
+
+Route::get('about', function (): Factory|View {
+    return view('about');
+});
+
+Route::get('/contact', function (): string {
+    return 'Contact Page!';
+});
+
+Route::get('/api', function (): array {
+    return ['foo' => 'bar'];
 });
