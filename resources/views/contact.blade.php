2c72596eb44073d90f2fedbb830b0f65fc1b55e6
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2026-01-04 11:47:25 +0500

    Added tailwind and x-slots

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,4 @@
-<x-layout title="Contact">
+<x-layout>
+    <x-slot:heading>Contact</x-slot:heading>
     <h1>Contact Page</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,4 @@
-<x-layout title="Home">
+<x-layout>
+    <x-slot:heading>Home</x-slot:heading>
     <h1>Home Page</h1>
 </x-layout>
