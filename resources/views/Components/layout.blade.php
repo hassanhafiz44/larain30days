46077fe5272d14e55732ee43fc2a41acc652a15f
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2026-01-04 11:51:07 +0500

    Added tailwind and x-slots (#2)

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -4,17 +4,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title }}</title>
+    <title>{{ $heading }}</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-    <nav>
-        <x-nav-link href="/">Home</x-nav-link>
-        <x-nav-link href="/about">About</x-nav-link>
-        <x-nav-link href="/contact">Contact</x-nav-link>
-    </nav>
+    <div class="min-h-full">
+        <nav class="bg-gray-800">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div class="flex h-16 items-center justify-between">
+                    <div class="flex items-center">
+                        <div class="flex shrink-0">
+                            <img class="h-8 w-8" src="https://laracasts.com/images/logo/logo-triangle.svg" alt="Logo">
+                        </div>
+                        <div class="hidden md:block">
+                            <div class="ml-10 flex items-baseline space-x-4">
+                                <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Home</a>
+                                <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+                                <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
 
-    {{ $slot }}
+        {{ $slot }}
+    </div>
 </body>
 
 </html>
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,4 @@
-<x-layout title="About">
+<x-layout>
+    <x-slot:heading>About</x-slot:heading>
     <h1>About Page</h1>
 </x-layout>
